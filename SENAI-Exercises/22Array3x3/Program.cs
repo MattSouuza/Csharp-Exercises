40f0e5c6eccf4a1ee80379f2164ad095d1ac83e0
MattSouuza <matheus_ss02@hotmail.com>
2019-10-29 12:11:17 -0300

    Arrays are boring and difficult

---
+++ SENAI-Exercises/22Array3x3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _22Array3x3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[,] randomArray = new int[3,3];
+
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    randomArray[i,j] = random.Next(1, 100);
+                    System.Console.WriteLine("Array[{0},{1}] = {2}", i, j, randomArray[i,j]);
+                }
+            }
+
+            int sum = randomArray.Length.Sum(x => Convert.ToInt32(x));
+            System.Console.WriteLine("O total é {0}", sum);
+        }
+    }
+}
---
+++ SENAI-Exercises/AgendaTel28/Registro.cs
@@ -0,0 +1,16 @@
+namespace AgendaTel28
+{
+    public class Registro
+    {
+        public string nome;
+        public string telefone;
+        public string aniversario;
+
+        public Registro(string nome, string telefone, string aniversario)
+        {
+            this.nome = nome;
+            this.telefone = telefone;
+            this.aniversario = aniversario;
+        }
+    }
+}
