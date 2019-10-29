b829a55cad5dc06f5f235d59fa1cacd75641957a
MattSouuza <matheus_ss02@hotmail.com>
2019-10-28 21:20:46 -0300

    Aaaaah

--- SENAI-Exercises/21Caixa/Program.cs
+++ SENAI-Exercises/21Caixa/Program.cs
@@ -6,10 +6,12 @@ namespace _21Caixa
     {
         static void Main(string[] args)
         {
-            int[] money = new int[] {2 , 5, 10, 20, 50, 100};
+            int[] money = new int[] {2, 5, 10, 20, 50, 100};
 
-            System.Console.WriteLine("Digite o valor");
+            System.Console.WriteLine("Digite o valor a ser sacado: ");
             int value = int.Parse(Console.ReadLine());
+
+            
         }
     }
 }
