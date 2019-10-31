98cf978eda673f66cc70fb70ee5410590f2b0650
Matheus Souza Silva <matheus_ss02@hotmail.com>
2019-10-31 16:55:41 -0300

    UOUOUOUOU

--- SENAI-Exercises/21Caixa/Program.cs
+++ SENAI-Exercises/21Caixa/Program.cs
@@ -14,12 +14,30 @@ namespace _21Caixa
             if(value > 0)
             {
                 int notas100 = value / money[0];
-                int resultado =  (value * money[0]); 
+                int resto =  value - (value * money[0]); 
 
-                resultado = resultado / money[1];
-                
+                int notas50 = resto / money[1];
+                resto = resto - (notas50 * money[1]);
+
+                int notas20 = resto / money[2];
+                resto = resto - (notas20 * money[2]);
+
+                int notas10 = resto / money[3];
+                resto = resto - (notas50 * money[3]);
+
+                int notas5 = resto / money[4];
+                resto = resto - (notas20 * money[4]);
+
+                int notas2 = resto / money[5];
+                resto = resto - (notas20 * money[5]);
+
+                System.Console.WriteLine($"Quantidade notas de 100 {notas100}.");
+                System.Console.WriteLine($"Quantidade notas de 50 {notas50}.");
+                System.Console.WriteLine($"Quantidade notas de 20 {notas20}.");
+                System.Console.WriteLine($"Quantidade notas de 10 {notas10}.");
+                System.Console.WriteLine($"Quantidade notas de 5 {notas5}.");
+                System.Console.WriteLine($"Quantidade notas de 2 {notas2}.");
             }
-            System.Console.WriteLine(value);
         }
     }
 }
