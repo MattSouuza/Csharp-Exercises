80b616bdd81045fc4552966e9facefc90b3edc21
MattSouuza <matheus_ss02@hotmail.com>
2019-11-06 12:56:37 -0300

    6/11

--- SENAI-Exercises/Zoologico/Program.cs
+++ SENAI-Exercises/Zoologico/Program.cs
@@ -9,16 +9,37 @@ namespace Zoologico
     {
         static void Main(string[] args)
         {
-            int opcaoNumber = 0;
-            string menuBar = "===========================";
-            System.Console.WriteLine(menuBar);
-            System.Console.WriteLine(" Zoológico de Bacanolândia");
-            System.Console.WriteLine(menuBar);
-
-            foreach(var item in Menu.menuAnimal.Values)
+            bool encerrouProgram = true;
+            do
             {
-                System.Console.WriteLine($"{opcaoNumber++} - {Values(item)}");
-            }
+                string menuBar = "=================================================================================";
+                System.Console.WriteLine(menuBar);
+                System.Console.WriteLine("|                         Zoológico de Bacanolândia                             |");
+                System.Console.WriteLine(menuBar);
+
+                foreach(var item in Menu.menuAnimal)
+                {
+                    System.Console.Write(item.Key + " - ");
+                    System.Console.WriteLine(item.Value.GetType().Name);
+                }
+                
+                System.Console.Write("\nSelecione um número correspondente a um animal: ");
+
+                try
+                {
+                    int opcaoUsuario = int.Parse(Console.ReadLine());
+                    System.Console.WriteLine();
+                    var animais = Menu.menuAnimal[opcaoUsuario];
+                    //Method here
+                    encerrouProgram = true;
+                }
+                catch (Exception)
+                {
+                    System.Console.WriteLine("Digite um número válido");
+                    Console.ReadLine();
+                    encerrouProgram = false;
+                }
+            } while(!encerrouProgram);
         }
     }
 }
