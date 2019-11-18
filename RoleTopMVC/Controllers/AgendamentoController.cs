5115a5c4de8f9b77761bbb47ac04a1b352ce570f
MattSouuza <matheus_ss02@hotmail.com>
2019-11-17 21:42:05 -0300

    MVClegal

--- RoleTopMVC/Controllers/AgendamentoController.cs
+++ RoleTopMVC/Controllers/AgendamentoController.cs
@@ -1,11 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RoleTopMVC.Models;
 
 namespace RoleTopMVC.Controllers
 {
     public class AgendamentoController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Agendar()
         {
+            ViewData["NomeView"] = "Agendamento";
             return View();
         }
     }
