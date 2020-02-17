30996bd09f4dc217e8829bcf90b346192002eded
MattSouuza <matheus_ss02@hotmail.com>
2020-02-17 16:06:03 -0300

    Api - Post

---
+++ sprint2/bd/senai-filmes-02-DML.sql
@@ -0,0 +1,10 @@
+USE Filmes_tarde;
+
+INSERT INTO Generos	(Nome)
+VALUES				('Ação')
+					,('Drama');
+					
+INSERT INTO Filmes	(Titulo, IdGenero)
+VALUES				('A vida é bela', 2)
+					,('Rambo', 1);
+
